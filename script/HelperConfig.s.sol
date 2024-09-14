1eea916d52181dd8dcd2ca7bb5c638dac473ff6c
Doomsday1102 <die2002alexsander@gmail.com>
2024-09-14 01:11:36 -0500

    Fisrt advance

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+// 1-DEploy Mock in Avil chain 
+// 2. Keep track of contracts address across diferents chains
+// Sepolia ETH/USD
+// MAINET ETH/USD
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol"
+
+contract HelperConfig {
+    //If we are on local anvil, we deploy mocks
+    //Otherwise, grab the existing address from the live network
+    
+
+}
